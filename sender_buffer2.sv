d042e22286e315bdafc9d681ac79daeb51d7be46
mamurai <gohome.x105.gn@gmail.com>
2016-11-21 08:30:26 +0900

    sender buffer2の修正

--- sender_buffer2.sv
+++ sender_buffer2.sv
@@ -21,28 +21,33 @@ module sender_buffer2 (
 			state <= 0;
 			buffer_enable <= 0;
 		end else begin
-			if (start != start_buffer) begin
+			if ((state == 0) && (start != start_buffer) && start) begin
 				start_buffer <= start;
-			end
-			if (start_buffer) begin
+				state <= state + 1;
+			end else if ((state == 0) && (start != start_buffer) && !start) begin
+				start_buffer <= start;
+			end else if ((state == 1) && start_buffer) begin
 				buffer <= data;
 				buffer_enable <= 1;
-			end
-			if ((state == 0) && sender_ready && buffer_enable) begin
+				state <= state + 1;
+			end if ((state == 2) && sender_ready && buffer_enable) begin
 				output_data <= buffer[31:24];
 				state <= state + 1;
 				valid <= 1;
-			end else if ((state == 1) && sender_ready) begin
+			end else if ((state == 3) && sender_ready) begin
 				output_data <= buffer[23:16];
 				state <= state + 1;
-			end else if ((state == 2) && sender_ready) begin
+			end else if ((state == 4) && sender_ready) begin
 				output_data <= buffer[15:8];
 				state <= state + 1;
-			end else if ((state == 3) && sender_ready) begin
+			end else if ((state == 5) && sender_ready) begin
 				output_data <= buffer[7:0];
 				state <= state + 1;
-			end else begin
+			end else if ((state >= 6) && (start != start_buffer)) begin
 				state <= 0;
+				valid <= 0;
+				buffer_enable <= 0;
+			end else if ((state >= 6) && (start == start_buffer)) begin
 				valid <= 0;
 				buffer_enable <= 0;
 			end
